e3d72eacc58146a17d0d8f3e85bf837025283924
İsa SAYAR <isa.sayar1725@gmail.com>
2022-04-24 16:06:50 +0300

    files edited.

--- vars/evenOdd.groovy
+++ vars/evenOdd.groovy
@@ -1,35 +1,3 @@
-
-Skip to content
-Product 
-Team
-Enterprise
-Explore 
-Marketplace
-Pricing 
-Search
-Sign in
-Sign up
-robertstarmer
-/
-evenOdd
-Public
-Code
-Issues
-Pull requests
-1
-Actions
-Projects
-Wiki
-Security
-Insights
-evenOdd/vars/evenOdd.groovy
-@robertstarmer
-robertstarmer Add initial code
-Latest commit 7d14afc on 7 Jun 2018
- History
- 1 contributor
-34 lines (34 sloc)  693 Bytes
-   
 // vars/evenOrOdd.groovy
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
